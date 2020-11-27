478894c8b63b1c67f3718b99e6b8dbb552452ced
Abner Pena <abnrr.pena@gmail.com>
2020-11-26 20:38:29 -0400

    signup writes to DB

--- app.py
+++ app.py
@@ -2,9 +2,6 @@
 
 from flask import Flask, render_template, redirect, url_for
 from flask_bootstrap import Bootstrap
-#from flask_wtf import FlaskForm 
-#from wtforms import StringField, PasswordField, BooleanField
-#from wtforms.validators import InputRequired, Email, Length
 from forms import LoginForm, RegisterForm
 from flask_sqlalchemy  import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -12,17 +9,15 @@ from flask_login import LoginManager, UserMixin, login_user, login_required, log
 
 #Initialize app
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = 'Thisissupposedtobesecret!'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
 
-login_manager = LoginManager()
-login_manager.init_app(app)
-login_manager.login_view = 'login'
+#login_manager = LoginManager()
+#login_manager.init_app(app)
+#login_manager.login_view = 'login'
 
 #Database Model
 class User(db.Model):
@@ -30,7 +25,6 @@ class User(db.Model):
     username = db.Column(db.String(15), unique=True)
     email = db.Column(db.String(50), unique=True)
     password = db.Column(db.String(80))
-
 
 @app.route('/')
 def index():
@@ -50,7 +44,12 @@ def signup():
     form = RegisterForm()
 
     if form.validate_on_submit():
-        return '<h1>'+form.username.data+' '+form.password.data+' '+form.email.data+'</h1>'
+        newUser = User(username=form.username.data, email=form.email.data, password=form.password.data)
+        db.session.add(newUser)
+        db.session.commit()
+        
+        return '<h1> Hello '+form.username.data+'!</h1>'
+        #return '<h1>'+form.username.data+' '+form.password.data+' '+form.email.data+'</h1>'
 
     return render_template('signup.html', form=form)
 
