05d4ecc1693b33117cc78559824da4c8e9362c70
Abner Pena <abnrr.pena@gmail.com>
2020-11-26 20:25:27 -0400

    sigin form posts/works

--- app.py
+++ app.py
@@ -54,13 +54,7 @@ def signup():
     form = RegisterForm()
 
     if form.validate_on_submit():
-        hashed_password = generate_password_hash(form.password.data, method='sha256')
-        new_user = User(username=form.username.data, email=form.email.data, password=hashed_password)
-        db.session.add(new_user)
-        db.session.commit()
-
-        return '<h1>New user has been created!</h1>'
-        #return '<h1>' + form.username.data + ' ' + form.email.data + ' ' + form.password.data + '</h1>'
+        return '<h1>'+form.username.data+' '+form.password.data+' '+form.email.data+'</h1>'
 
     return render_template('signup.html', form=form)
 
