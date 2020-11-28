b7408dcc75d3a055ff1bfefcf877280fc5031c51
Abner Pena <abnrr.pena@gmail.com>
2020-11-28 17:44:45 -0400

    finished database model for User and Task relation

--- app.py
+++ app.py
@@ -51,7 +51,9 @@ class User(UserMixin,db.Model): #Add UserMixin to DataBase Model
     password = db.Column(db.String(80),nullable=False)
     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     
-    tasks = db.relationship('Task',backref='creatorOfTask',lazy=True) #lazy means sqlalchemy will load the data from the database automatically
+    tasks = db.relationship('Task',backref='creatorOfTask',lazy=True) 
+    #Task references actually model class
+    #lazy means sqlalchemy will load the data from the database automatically
     
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
@@ -60,6 +62,8 @@ class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(150),nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False) 
+    #ForeignKey references actual table name and column name hence user(table).id(column)
     
     def __repr__(self):
         return f"Task('{self.text}', '{self.date_posted}')"
