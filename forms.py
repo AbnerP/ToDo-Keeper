a08b6aba24a83c53ed0e647df1eb74ddb5ed519e
Abner Pena <abnrr.pena@gmail.com>
2020-11-28 16:14:12 -0400

    confirm password on signup works

--- forms.py
+++ forms.py
@@ -1,14 +1,15 @@
 from flask_wtf import FlaskForm 
 from wtforms import StringField, PasswordField, BooleanField
-from wtforms.validators import InputRequired, Email, Length
+from wtforms.validators import DataRequired, InputRequired, Email, Length, EqualTo
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[InputRequired(), Length(min=4, max=15)])
-    password = PasswordField('Password', validators=[InputRequired(), Length(min=8, max=80)])
+    username = StringField('Username', validators=[DataRequired(),InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('Password', validators=[DataRequired(),InputRequired(), Length(min=8, max=80)])
     remember = BooleanField('Remember me')
 
 class RegisterForm(FlaskForm):
-    email = StringField('Email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
-    username = StringField('Username', validators=[InputRequired(), Length(min=4, max=15)])
-    password = PasswordField('Password', validators=[InputRequired(), Length(min=8, max=80)])
+    email = StringField('Email', validators=[DataRequired(), InputRequired(), Email(message='Invalid email'), Length(max=50)])
+    username = StringField('Username', validators=[DataRequired(),InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('Password', validators=[DataRequired(),InputRequired(), Length(min=8, max=80)])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(),InputRequired(), Length(min=8, max=80),EqualTo('password')])
     remember = BooleanField('Remember me')
