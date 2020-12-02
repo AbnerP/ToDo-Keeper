0db6450e107e8d96f367daa4886a01d41d79130c
Abner Pena <abnrr.pena@gmail.com>
2020-12-02 18:09:07 -0400

    Update and Delete work

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,10 @@
 import os
+from os import abort
 import secrets
 from app import app,db,login_manager
 from app.models import User,Task
 from app.forms import LoginForm, RegisterForm, TaskForm, UpdateAccoountForm
-from flask import render_template, url_for, redirect, session,flash
+from flask import render_template, url_for, redirect, session, flash, abort
 from flask.globals import request
 from flask_login import login_user, login_required, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -88,26 +89,52 @@ def dashboard():
     
     return render_template('dashboard.html',userStatus=userStatus,tasks=current_user.tasks,name=current_user.username ,form=form)
 
-@app.route('/dashboard/task/new', methods=['GET','POST'])
+@app.route('/task/<int:task_id>', methods=['GET','POST'])
 @login_required
-def new_task():
+def task(task_id):
+    task = Task.query.get_or_404(task_id)
+    userStatus = current_user.is_active
+    if task.user_id != current_user.id:
+        abort(403)
     form = TaskForm()
+    form.text.data = task.text
+    return render_template('task.html',taskid=task_id,userStatus=userStatus,task=task,name=current_user.username)
+
+@app.route('/task/<int:task_id>/update', methods=['GET','POST'])
+@login_required
+def update_task(task_id):
+    task = Task.query.get_or_404(task_id)
     userStatus = current_user.is_active
     
-    if form.validate_on_submit():
-        newTask = Task(text=form.text.data, user_id=current_user.id)
-        
-        db.session.add(newTask)
-        db.session.commit()
-        
-        print("NEW TASK CREATED -'"+str(newTask)+"'")
-        flash(f'{current_user.username} created a task!','success')
-        return redirect(url_for('dashboard',userStatus=userStatus,tasks=current_user.tasks,name=current_user.username,form=form))
-        #return '<h1> Hello '+form.username.data+'!</h1>'
+    if task.user_id != current_user.id:
+        abort(403)
     
-    return render_template('dashboard.html',userStatus=userStatus,tasks=current_user.tasks,name=current_user.username ,form=form)
+    form = TaskForm()
+    
+    if form.validate_on_submit():
+        task.text = form.text.data
+        db.session.commit()
+        return redirect(url_for('task',task_id=task_id,))
+    
+    elif request.method == 'GET':
+        form.text.data = task.text
+        
+    return render_template('taskUpdate.html',userStatus=userStatus,task=task,name=current_user.username,form=form)
 
-
+@app.route('/task/<int:task_id>/delete', methods=['POST'])
+@login_required
+def delete_task(task_id):
+    task = Task.query.get_or_404(task_id)
+    userStatus = current_user.is_active
+    
+    if task.user_id != current_user.id:
+        abort(403)
+    
+    db.session.delete(task)
+    db.session.commit()
+    
+    return redirect(url_for('dashboard'))
+    
 
 def save_picture(form_picture):
     random_hex = secrets.token_hex(8)
